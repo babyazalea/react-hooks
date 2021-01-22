e4457a2f3a3cceff1701f6c38a4779d9a842d15b
babyazalea <usun12@hotmail.com>
2021-01-22 11:52:45 +0900

    communicate with each other component via useState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -9,7 +9,7 @@ const IngredientForm = React.memo((props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddingredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
